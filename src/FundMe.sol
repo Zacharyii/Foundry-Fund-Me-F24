0c040d101d660e3cb7b18ea5a691782f06d97779
Zacharyii <1903253513@qq.com>
2024-08-04 20:19:48 +0800

    添加注释

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// Note: The AggregatorV3Interface might be at a different location than what was in the video!
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
@@ -10,13 +9,12 @@ error FundMe_NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public s_addressToAmountFunded;
-    address[] public s_funders;
+    mapping(address => uint256) public s_addressToAmountFunded; //存储每个地址提供的资金数量
+    address[] public s_funders; //存储所有提供资金的地址
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address private immutable i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
-    AggregatorV3Interface private s_priceFeed;
+    address private immutable i_owner; //存储合约所有者地址
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18; //设置最低提供的美元价值（以Wei为单位）
+    AggregatorV3Interface private s_priceFeed; //存储价格预言机的接口实例，一个合约地址
 
     constructor(address priceFeed) {
         i_owner = msg.sender;
@@ -28,18 +26,19 @@ contract FundMe {
             msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+
         s_addressToAmountFunded[msg.sender] += msg.value;
         s_funders.push(msg.sender);
     }
 
+    // 获取预言机版本
     function getVersion() public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeed);
         return s_priceFeed.version();
     }
 
+    // 权限控制修饰符，确保只有合约所有者才能调用特定函数
     modifier onlyOwner() {
-        // require(msg.sender == owner);
         if (msg.sender != i_owner) revert FundMe_NotOwner();
         _;
     }
@@ -71,24 +70,19 @@ contract FundMe {
             s_addressToAmountFunded[funder] = 0;
         }
         s_funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
 
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-
-        // call
         (bool callSuccess, ) = payable(msg.sender).call{
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
     }
 
+    // 处理接收到的 ETH
     fallback() external payable {
         fund();
     }
 
+    // 处理接收到的 ETH
     receive() external payable {
         fund();
     }
--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -3,29 +3,27 @@ pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-// Why is this a library and not abstract?
-// Why not an interface?
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
+    // 获取当前的ETH/USD价格
     function getPrice(
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
-        // Sepolia ETH / USD Address
-        // https://docs.chain.link/data-feeds/price-feeds/addresses
-
+        // 调用Chainlink的价格馈送合约获取最新一轮的数据
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
+
+        // 将价格转换为18位小数的整数格式返回
         return uint256(answer * 10000000000);
     }
 
-    // 1000000000
+    // 将ETH金额转换为USD金额
     function getConversionRate(
         uint256 ethAmount,
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
+        // 获取当前的ETH价格
         uint256 ethPrice = getPrice(priceFeed);
+        // 计算ethAmount对应的USD金额
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
-        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
 }
